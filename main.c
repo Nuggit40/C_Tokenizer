96cc41e4beb56b6730437a5cbd9886681c8c3138
Daniel C. Teffenhart <dct68@rutgers.edu>
2020-09-18 20:29:57 -0400

    added basic loop to main.c

--- main.c
+++ main.c
@@ -2,7 +2,21 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include "token.h"
+#include <string.h>
+void printTokens(char* input){
+	int i = 0;
+	while(input[i] != '\0'){
+		//Create a token struct
+		// determine type of token
+		// interrogate input futher to find where the token ends given the token type
+		// print out the token and advance i to the index where the last token ended
+		++i;
+	}
+}
 int main(int argc, char* argv[]){
-	printf("Hello World!\n");
-
+	if (argc == 2){
+		printTokens(argv[1]);
+	} else {
+		printf("Invalid argument count\n");
+	}
 }
