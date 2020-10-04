516adc43d6489d5ee7c57168693cd980655b494e
WKeleher <wkeleher41@gmail.com>
2020-10-04 17:52:25 -0400

    keyword type addition

--- tokenizer.c
+++ tokenizer.c
@@ -3,8 +3,42 @@
 #include <ctype.h>
 #include <string.h>
 typedef enum _tokenType{
-	word, decimalInt, octalInt, hexadecimalInt, floatInt, op
+	word, decimalInt, octalInt, hexadecimalInt, floatInt, op, kw
 } tokenType;
+typedef enum _keyword{
+	_char,
+	_auto,	
+	_double,
+	_int,
+	_struct,
+	_break,
+	_else,
+	_long,
+	_switch,
+	_case,
+	_enum,
+	_register,
+	_typedef,
+	_extern,
+	_return,
+	_union,
+	_continue,
+	_for,
+	_signed,
+	_void,
+	_do,
+	_if,
+	_static,
+	_while,
+	_default,
+	_goto,
+	_volatile,
+	_const,
+	_float,
+	_short,
+	_unsigned,
+
+} keywordType;
 typedef enum _operatorType{
 	left_parenthesis,
 	right_parenthesis,
@@ -49,13 +83,6 @@ typedef enum _operatorType{
 	AND_address_operator,
 	minus_subtract_operator,
 	multiply_dereference_operator,
-	_char,
-	signed_char,
-	signed_short_int,
-	signed_short,
-	singed_long_int,
-	singed_long_long_int,
-	singed_long_long,
 	unknown
 } operatorType;
 typedef struct _token{
@@ -63,6 +90,7 @@ typedef struct _token{
 	int endIndex;
 	tokenType type;
 	operatorType opType;
+	keywordType kwType;
 } token;
 //Finds the type and size of an operator and checks for unsupported operators
 void processOperator(token* tok, char* input){
@@ -350,56 +378,191 @@ void processWord(token* tok, char* input){
 		tok->opType = sizeOf;
 		return;
 	}
+	else if(c[0] == 'a' && c[1] == 'u' && c[2] == 't' && c[3] == 'o'){
+		tok->endIndex += 4;
+		tok->type = kw;
+		tok->kwType = _auto;
+		return;
+	}
+	else if(c[0] == 'b' && c[1] == 'r' && c[2] == 'e' && c[3] == 'a' && c[4] == 'k'){
+		tok->endIndex += 5;
+		tok->type = kw;
+		tok->kwType = _break;
+		return;
+	}
+	else if(c[0] == 'c' && c[1] == 'a' && c[2] == 's' && c[3] == 'e'){
+		tok->endIndex += 4;
+		tok->type = kw;
+		tok->kwType = _case;
+		return;
+	}
+	else if(c[0] == 'd' && c[1] == 'o'){
+		tok->endIndex += 2;
+		tok->type = kw;
+		tok->kwType = _do;
+		return;
+	}
+	else if(c[0] == 'c' && c[1] == 'o' && c[2] == 'n' && c[3] == 't' && c[4] == 'i' && c[5] == 'n' && c[6] == 'u' && c[7] == 'e'){
+		tok->endIndex += 8;
+		tok->type = kw;
+		tok->kwType = _continue;
+		return;
+	}
+	else if(c[0] == 'd' && c[1] == 'e' && c[2] == 'f' && c[3] == 'a' && c[4] == 'u' && c[5] == 'l' && c[6] == 't'){
+		tok->endIndex += 7;
+		tok->type = kw;
+		tok->kwType = _default;
+		return;
+	}
+	else if(c[0] == 'c' && c[1] == 'o' && c[2] == 'n' && c[3] == 's' && c[4] == 't'){
+		tok->endIndex += 5;
+		tok->type = kw;
+		tok->kwType = _const;
+		return;
+	}
+	else if(c[0] == 'd' && c[1] == 'o' && c[2] == 'u' && c[3] == 'b' && c[4] == 'l' && c[5] == 'e'){
+		tok->endIndex += 6;
+		tok->type = kw;
+		tok->kwType = _double;
+		return;
+	}
+	else if(c[0] == 'e' && c[1] == 'l' && c[2] == 's' && c[3] == 'e'){
+		tok->endIndex += 4;
+		tok->type = kw;
+		tok->kwType = _else;
+		return;
+	}
+	else if(c[0] == 'e' && c[1] == 'n' && c[2] == 'u' && c[3] == 'm'){
+		tok->endIndex += 4;
+		tok->type = kw;
+		tok->kwType = _enum;
+		return;
+	}
+	else if(c[0] == 'e' && c[1] == 'x' && c[2] == 't' && c[3] == 'e' && c[4] == 'r' && c[5] == 'n'){
+		tok->endIndex += 6;
+		tok->type = kw;
+		tok->kwType = _extern;
+		return;
+	}
+	else if(c[0] == 'f' && c[1] == 'o' && c[2] == 'r'){
+		tok->endIndex += 3;
+		tok->type = kw;
+		tok->kwType = _for;
+		return;
+	}
+	else if(c[0] == 'i' && c[1] == 'f'){
+		tok->endIndex += 2;
+		tok->type = kw;
+		tok->kwType = _if;
+		return;
+	}
+	else if(c[0] == 'g' && c[1] == 'o' && c[2] == 't' && c[3] == 'o'){
+		tok->endIndex += 4;
+		tok->type = kw;
+		tok->kwType = _goto;
+		return;
+	}
+	else if(c[0] == 'f' && c[1] == 'l' && c[2] == 'o' && c[3] == 'a' && c[4] == 't'){
+		tok->endIndex += 5;
+		tok->type = kw;
+		tok->kwType = _float;
+		return;
+	}
+	else if(c[0] == 'i' && c[1] == 'n' && c[2] == 't'){
+		tok->endIndex += 3;
+		tok->type = kw;
+		tok->kwType = _int;
+		return;
+	}
 	else if(c[0] == 'c' && c[1] == 'h' && c[2] == 'a' && c[3] == 'r'){
 		tok->endIndex +=4;
 		tok->type = op;
 		tok->opType = _char;
 	}
 	else if(c[0] == 's' && c[1] == 'i' && c[2] == 'g' && c[3] == 'n' && c[4] == 'e' && c[5] == 'd'){
-		tok->endIndex += 6;	
-		if(isspace (c[6]) && c[7] == 'c' && c[8] == 'h' && c[9] == 'a' && c[10] == 'r'){
-			tok->endIndex +=5;
-			tok->type = op;
-			tok->opType = signed_char;
+		tok->endIndex += 6;
+		tok->type = kw;
+		tok->kwType = _signed;
+		return;	
 	}
+	else if(c[0] == 's' && c[1] == 'h' && c[2] == 'o' && c[3] == 'r'&& c[4] == 't' ){
+		tok->endIndex += 5;
+		tok->type = kw;
+		tok->kwType = _short;
+		return;
 	}
-	else if(isspace (c[6]) && c[7] == 's' && c[8] == 'h' && c[9] == 'o' && c[10] == 'r'&& c[11] == 't' ){
-		tok->endIndex +=6;
-			if(isspace (c[12]) && c[13] == '1' && c[14] == 'n' && c[15] == 't'){
-				tok->endIndex +=4;
-				tok->type = op;
-				tok->opType = signed_short_int;	
-
-		}
-		else{
-			tok->type = op;
-			tok->opType = signed_short;	
-		}
+	else if(c[0] == 'l' && c[1] == 'o' && c[2] == 'n' && c[3] == 'g'){
+		tok->endIndex += 4;
+		tok->type = kw;
+		tok->kwType = _long;
+		return;
 	}
-	else if(isspace (c[6]) && c[7] == 'l' && c[8] == 'o' && c[9] == 'n' && c[10] == 'g'){
-		tok->endIndex +=5;
-		if(isspace (c[11]) && c[12] == '1' && c[13] == 'n' && c[14] == 't'){
-			tok->endIndex +=4;
-			tok->type = op;
-			tok->opType = singed_long_int;	
-
-		}
-		else if(isspace (c[11]) && c[12] == 'l' && c[13] == 'o' && c[14] == 'n' && c[15] == 'g'){
-		tok->endIndex +=5;
-		if(isspace (c[16]) && c[17] == 'i' && c[18] == 'n' && c[19] == 't'){
-			tok->endIndex +=4;
-			tok->type = op;
-			tok->opType = singed_long_long_int;	
-
-		}
-		else{
-			tok->type = op;
-			tok->opType = singed_long_long;	
-		}
-	}				
-	
+	else if(c[0] == 'u' && c[1] == 'n' && c[2] == 's' && c[3] == 'i' && c[4] == 'g' && c[5] == 'n' && c[6] == 'e' && c[7] == 'd'){
+		tok->endIndex += 8;
+		tok->type = kw;
+		tok->kwType = _unsigned;
+		return;
 	}
-	
+	else if(c[0] == 'v' && c[1] == 'o' && c[2] == 'l' && c[3] == 'a' && c[4] == 't' && c[5] == 'i' && c[6] == 'l' && c[7] == 'e'){
+		tok->endIndex += 8;
+		tok->type = kw;
+		tok->kwType = _volatile;
+		return;
+	}
+	else if(c[0] == 'w' && c[1] == 'h' && c[2] == 'i' && c[3] == 'l' && c[4] == 'e'){
+		tok->endIndex += 5;
+		tok->type = kw;
+		tok->kwType = _while;
+		return;
+	}
+	else if(c[0] == 'v' && c[1] == 'o' && c[2] == 'i' && c[3] == 'd'){
+		tok->endIndex += 4;
+		tok->type = kw;
+		tok->kwType = _void;
+		return;
+	}
+	else if(c[0] == 'u' && c[1] == 'n' && c[2] == 'i' && c[3] == 'o' && c[4] == 'n'){
+		tok->endIndex += 5;
+		tok->type = kw;
+		tok->kwType = _union;
+		return;
+	}
+	else if(c[0] == 't' && c[1] == 'y' && c[2] == 'p' && c[3] == 'e' && c[4] == 'd' && c[5] == 'e' && c[6] == 'f'){
+		tok->endIndex += 7;
+		tok->type = kw;
+		tok->kwType = _typedef;
+		return;
+	}
+	else if(c[0] == 's' && c[1] == 'w' && c[2] == 'i' && c[3] == 't' && c[4] == 'c' && c[5] == 'h'){
+		tok->endIndex += 6;
+		tok->type = kw;
+		tok->kwType = _switch;
+		return;
+	}
+	else if(c[0] == 's' && c[1] == 't' && c[2] == 'r' && c[3] == 'u' && c[4] == 'c' && c[5] == 't'){
+		tok->endIndex += 6;
+		tok->type = kw;
+		tok->kwType = _struct;
+		return;
+	}
+	else if(c[0] == 's' && c[1] == 't' && c[2] == 'a' && c[3] == 't' && c[4] == 'i' && c[5] == 'c'){
+		tok->endIndex += 6;
+		tok->type = kw;
+		tok->kwType = _static;
+		return;
+	}
+	else if(c[0] == 'r' && c[1] == 'e' && c[2] == 't' && c[3] == 'u' && c[4] == 'r' && c[5] == 'n'){
+		tok->endIndex += 6;
+		tok->type = kw;
+		tok->kwType = _return;
+		return;
+	}
+	else if(c[0] == 'r' && c[1] == 'e' && c[2] == 'g' && c[3] == 'i' && c[4] == 's' && c[5] == 't' && c[6] == 'e' && c[7] == 'r'){
+		tok->endIndex += 8;
+		tok->type = kw;
+		tok->kwType = _register;
+		return;
+	}
 	//keep reading characters and incrementing the tokens ending index
 	//until a character is not allowed to be in a word type
 	while(isalnum(*c)){
@@ -407,6 +570,102 @@ void processWord(token* tok, char* input){
 		++c;
 	}
 }
+char* getKeywordString(token* tok){
+	switch(tok->kwType){
+		case _auto:
+			return "auto";
+			break;
+		case _break:
+			return "break";
+			break;
+		case _case:
+			return "case";
+			break;
+		case _char:
+			return "char";
+			break;
+		case _continue:
+			return "continue";
+			break;
+		case _do:
+			return "do";
+			break;
+		case _default:
+			return "default";
+			break;
+		case _const:
+			return "const";
+			break;
+		case _double:
+			return "double";
+			break;
+		case _else:
+			return "else";
+			break;
+		case _enum:
+			return "enum";
+			break;
+		case _extern:
+			return "extern";
+			break;
+		case _for:
+			return "for";
+			break;
+		case _if:
+			return "if";
+			break;
+		case _goto:
+			return "goto";
+			break;
+		case _float:
+			return "float";
+			break;
+		case _int:
+			return "int";
+			break;
+		case _long:
+			return "long";
+			break;
+		case _register:
+			return "register";
+			break;
+		case _signed:
+			return "signed";
+			break;
+		case _static:
+			return "static";
+			break;
+		case _short:
+			return "short";
+			break;
+		case _struct:
+			return "struct";
+			break;
+		case _switch:
+			return "switch";
+			break;
+		case _typedef:
+			return "typedef";
+			break;
+		case _union:
+			return "union";
+			break;
+		case _void:
+			return "void";
+			break;
+		case _while:
+			return "while";
+			break;
+		case _volatile:
+			return "volatile";
+			break;
+		case _unsigned:
+			return "unsigned";
+			break;
+		default:
+			return "";
+	}
+	}				
 //Assumes what the initial type should be of the current token and sets it
 void processInitialChar(token* tok, char* input) {
 	char c = input[tok->startIndex];
@@ -559,27 +818,6 @@ char* getOperatorString(token* tok){
 			break;
 		case multiply_dereference_operator:
 			return "multiply/dereference operator";
-			break;
-		case _char:
-			return "char";
-			break;
-		case signed_char:
-			return "singed char";
-			break;
-		case signed_short_int:
-			return "signed short int";
-			break;
-		case signed_short:
-			return "signed short";
-			break;
-		case singed_long_int:
-			return "signed long int";
-			break;
-		case singed_long_long_int:
-			return "signed long long int";
-			break;
-		case singed_long_long:
-			return "signed long long";
 			break;
 		case unknown:
 			return "unknown character";
